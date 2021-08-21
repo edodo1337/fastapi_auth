e47baabf07d57f74254e907453b5923b1dc283be
Eldar <eldar100@mail.ru>
2021-08-21 13:12:15 +0300

    init

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 from __future__ import annotations
 
+import fastapi
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get('/')
-async def root():
+async def root() -> fastapi.Response:
     return {'message': 'Hello'}
 
 
-def test(response):
+def test(response: int) -> int:
     return response + 1
