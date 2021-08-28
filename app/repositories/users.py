7e1d55c8a63ac4d885a56f6214a12db146fccec3
Eldar <eldar100@mail.ru>
2021-08-28 21:19:33 +0300

    userrepo fix

--- app/repositories/users.py
+++ app/repositories/users.py
@@ -15,8 +15,13 @@ class UsersRepository(BaseRepository):
     def delete(self, pk: int) -> None:
         pass
 
-    def get_by_id(self, pk: int) -> Optional[User]:
-        pass
+    async def get_by_id(self, id: int) -> Optional[User]:
+        query = 'SELECT * FROM users WHERE id = :id LIMIT 1'
+        values = {'id': id}
+        data = await self.db.fetch_one(query=query, values=values)
+        if data is None:
+            return None
+        return User(**data)
 
     async def get_by_username(self, username: str) -> Optional[User]:
         query = 'SELECT * FROM users WHERE username = :username LIMIT 1'
@@ -37,5 +42,5 @@ class UsersRepository(BaseRepository):
             raise UserUniqueException
         return data
 
-    def update(self, pk: int, data: User) -> None:
+    def update(self, id: int, data: User) -> None:
         return None
